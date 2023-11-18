9755aaacbdcce1773bbff3efeaf84b2a86a65a14
bill <billie.tt@gmail.com>
2023-11-17 21:22:52 -0500

    nov17 2023

--- index.php
+++ index.php
@@ -3,16 +3,17 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Phpml\Metric\Regression;
+use Phpml\Metric\Accuracy;
 use Phpml\Dataset\CsvDataset;
-use Phpml\Regression\LeastSquares;
-use Phpml\CrossValidation\RandomSplit;
+use Phpml\Regression\SVR;
+use Phpml\CrossValidation\StratifiedRandomSplit;
 
 //loading the data
 
-$data = new CsvDataset("./data/insurance.csv", 1, true);
+$data = new CsvDataset("./data/wine.csv", 13, true);
 
 //preprocessing data
-$dataset = new RandomSplit($data, 0.2, 156);
+$dataset = new StratifiedRandomSplit($data, 0.2, 156);
 
 // $dataset->getTrainSamples();
 // $dataset->getTrainLabels();
@@ -20,18 +21,26 @@ $dataset = new RandomSplit($data, 0.2, 156);
 // $dataset->getTestLabels();
 
 //training
-$regression = new LeastSquares();
+$regression = new SVR();
 $regression->train($dataset->getTrainSamples(), $dataset->getTrainLabels());
 
-$predict = $regression->predict( $dataset->getTestSamples());
+$predicted = $regression->predict( $dataset->getTestSamples());
 
 //evaluating machine learning models
-$score = Regression::r2Score($dataset->getTestLabels(), $predict);
-echo "r2score is : ". $score;  // % accuracy
+$score = Regression::r2Score($dataset->getTestLabels(), $predicted);
+echo "r2score is : ". $score .PHP_EOL;
 
-//making predictions with trained models
-echo "\n";
-var_dump($regression->predict([80]));
+foreach($predicted as &$target){
+    $target = round($target,0);
+}
+
+
+$accuracy = Accuracy::score($dataset->getTestLabels(),$predicted);
+echo 'accuracy is: '. $accuracy;
+
+// //making predictions with trained models
+// echo "\n";
+// var_dump($regression->predict([80]));
 
 
 
